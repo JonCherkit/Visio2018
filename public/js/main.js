5845022f8332391c867fd6f3bc0f0488dbc317cc
Jonathan CHERKIT <j.cherkit@gmail.com>
2018-01-19 12:05:27 +0100

    Ajout du squelette de la fonction JS pour 'NextButton', Corps de la fonction TODO

--- public/js/main.js
+++ public/js/main.js
@@ -20,7 +20,13 @@ document.addEventListener('DOMContentLoaded',function(){
 	}
 
 	//--------- Bouton "Next_Image" --------
-	
+	var nextButtonSelector = document.querySelectorAll('.nextButton')[0];	//il n'y à qu'un seul nbouton next sur la page, alors on prend le premier élément de la collection 
+	// console.log(nextButtonNode);	//debug
+	nextButtonSelector.addEventListener('click',function(){
+		// To do : Savoir sur quelle image on se trouve pour savoir quelle est la suivante/précédente
+		// - trouver l'adresse de l'image suivante,
+		// - modifier l'attribut source de la zone d'affichage avec le lien de l'image 'suivante'
+	});
 
 
 // ------------ Partie AJAX ------------
@@ -31,7 +37,7 @@ xhr.open('GET','public/js/images.json'); 	//Demande au server du fichier images.
 // Listener sur l'évènement 'load' ,récupère, parse et affiche en console le contenu du fichier /public/js/images.json
 xhr.addEventListener('load',function(){
 	var responseData = JSON.parse(this.responseText);
-	console.log(responseData);
+	// console.log(responseData);	//Debug
 });
 
 xhr.send()	//Envois de la requete
